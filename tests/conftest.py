7af401f1636873bfee3e815ea2fc771b358e3081
Jeffrey Wright <jeffrey.wright@wartsila.com>
2021-07-04 14:24:27 -0400

    Testing hooks...

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
 pytest_plugins = 'pytester'
+
+def pytest_runtest_setup(item):
+    print("setting up:", item)
+
+def pytest_runtest_call(item):
+    print("calling:", item)
+
+def pytest_runtest_collect(item):
+    print("collecting:", item)
+
+def pytest_runtest_teardown(item):
+    print("tearing down:", item)
+
