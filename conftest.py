1f4197366b82644a109e0183604694ba3923b6f3
Jeff Wright <jeff.washcloth@gmail.com>
2021-07-05 01:40:53 -0400

    Moved conftest to / as per pytest_sugar.

--- conftest.py
+++ conftest.py
@@ -2,6 +2,13 @@ import pytest
 
 
 pytest_plugins = "pytester"
+collect_ignore = [
+    "pytest_fold.py",
+    "setup.py",
+    "pytest_something.py",
+    "pytest_capture.py",
+    "*.yml",
+]
 
 
 def pytest_configure(config):
