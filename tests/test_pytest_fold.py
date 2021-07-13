9fbf34527480e1924891e790c0ac39e56c867a44
Jeff Wright <jeff.washcloth@gmail.com>
2021-07-13 03:19:57 -0400

    One pass, one fail, with output.

--- tests/test_pytest_fold.py
+++ tests/test_pytest_fold.py
@@ -1,54 +1,87 @@
+import sys
 import time
+import logging
 import pytest
 from pytest_fold import __version__
 
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+logger.propagate = True
+# stdout_handler = logging.StreamHandler(sys.stdout)
+# logger.addHandler(stdout_handler)
 
-def test_version():
-    assert __version__ == "0.1.0"
+# def test_version():
+#     assert __version__ == "0.1.0"
 
 
 # def test_which_fails_1():
 #     assert 0
 
 
-def test_which_passes_1():
-    assert 1 == 1
+# def test_which_passes_1():
+#     assert 1 == 1
 
 
 # def test_which_fails_2():
 #     assert 0
 
 
-def test_which_passes_2():
-    assert 1 == 1
+# def test_which_passes_2():
+#     assert 1 == 1
 
 
 # def test_which_fails_3():
 #     assert 0
 
 
-def test_which_passes_3():
-    assert 1 == 1
+# def test_which_passes_3():
+#     assert 1 == 1
 
+
+def test_fail_capturing(capsys):
+    print("\nFAIL this stdout is captured")
+    sys.stderr.write("FAIL this stderr is captured\n")
+    logger.warning("FAIL this log is captured")
+    with capsys.disabled():
+        print("FAIL stdout not captured, going directly to sys.stdout")
+        sys.stderr.write("FAIL stderr not captured, going directly to sys.stderr\n")
+        logger.warning("FAIL is this log captured?")
+    print("FAIL this stdout is also captured")
+    sys.stderr.write("FAIL this stderr is also captured\n")
+    logger.warning("FAIL this log is also captured")
+    assert False
+
+def test_pass_capturing(capsys):
+    print("\nPASS this stdout is captured")
+    sys.stderr.write("PASS this stderr is captured\n")
+    logger.warning("PASS this log is captured")
+    with capsys.disabled():
+        print("PASS stdout not captured, going directly to sys.stdout")
+        sys.stderr.write("PASS stderr not captured, going directly to sys.stderr\n")
+        logger.warning("is this log captured?")
+    print("PASS this stdout is also captured")
+    sys.stderr.write("PASS this stderr is also captured\n")
+    logger.warning("PASS this log is also captured")
+    assert True
 
 # def test_which_fails_and_has_stdout_1(capsys):
 #     print("this test fails")
 #     assert 0 == 1
 
 
-def test_which_passes_and_pauses_and_has_stdout_1(capsys):
-    print("this test pauses, then passses")
-    time.sleep(2)
-    assert -11 == -11
+# def test_which_pauses_and_fails_and_has_stdout_1(capsys):
+#     print("this test pauses, then passses")
+#     time.sleep(2)
+#     assert 0 == -11
 
 
-def test_which_passes_and_has_stdout_2(capsys):
-    print("this test passes")  # stdout is consumed by pytest
-    assert "a" == "a"
+# def test_which_passes_and_has_stdout_2(capsys):
+#     print("this test passes")  # stdout is consumed by pytest
+#     assert "a" == "a"
 
 
-def test_summary():
-    assert "a" == "a"
+# def test_summary():
+#     assert "a" == "a"
 
 
 # def test_hello(hello):
