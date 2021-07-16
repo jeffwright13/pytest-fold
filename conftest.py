a61d60f0f391bb4692b5b2e8c79b562bf4eae472
Jeff Wright <jeff.washcloth@gmail.com>
2021-07-16 06:27:25 -0400

    Added wrapper to pytest_runtest_makereport hook so that pytest_runtest_logreport can access the global request.config object and see if --fold is set on command line options.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 collect_ignore = [
     "pytest_fold.py",
     "setup.py",
@@ -9,15 +10,33 @@ collect_ignore = [
 ]
 
 
+# Register pytest-fold option (--fold)
+def pytest_addoption(parser):
+    group = parser.getgroup("fold")
+    group.addoption(
+        "--fold", action="store_true", help="fold: fold failed test output sections"
+    )
+
+
+@pytest.fixture(autouse=True)
+def fold(request):
+    return request.config.getoption("--fold")
+
+
+# attach session info to report for later use in pytest_runtest_logreport
+# https://stackoverflow.com/questions/54717786/access-pytest-session-or-arguments-in-pytest-runtest-logreport
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    out = yield
+    report = out.get_result()
+    report.session = item.session
+
+
 @pytest.hookimpl(trylast=True, hookwrapper=True)
 def pytest_runtest_logreport(report):
     yield
-    if report.when == "call":
-        if report.failed:
-            # breakpoint()
-            # if fold:
-            #     print("Test failed; overridden by fold plugin.", file=sys.stderr)
+    if report.when == "call" and report.failed:
+        # breakpoint()
+        if report.session.config.option.fold:
             report.longrepr.chain[0][0].reprentries[0].lines.insert(0, "===MARKER1===")
-            # report.longrepr.chain[0][0].reprentries[0].reprfileloc.message += "===MARKER2==="
             report.longrepr.chain[0][0].extraline = "===MARKER2==="
-            pass
