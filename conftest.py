5a2815f114b1d358c670f47d74e963e54e31f2ee
Jeff Wright <jeffrey.wright@wartsila.com>
2022-01-18 21:45:52 -0700

    Added a bunch of ouput files to analyze (each from runs with difference runtime options).

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-pytest_plugins = 'pytest_session2file'
+pytest_plugins = "pytest_session2file"
 
 collect_ignore = [
     "pytest_fold.py",
@@ -17,9 +17,11 @@ def pytest_addoption(parser):
         "--fold", action="store_true", help="fold: fold failed test output sections"
     )
 
+
 @pytest.fixture(autouse=True)
 def fold(request):
     return request.config.getoption("--fold")
+
 
 # attach session info to report for later use in pytest_runtest_logreport
 # https://stackoverflow.com/questions/54717786/access-pytest-session-or-arguments-in-pytest-runtest-logreport
@@ -29,13 +31,10 @@ def pytest_runtest_makereport(item, call):
     report = out.get_result()
     report.session = item.session
 
+
 @pytest.hookimpl(trylast=True, hookwrapper=True)
 def pytest_runtest_logreport(report):
     yield
-    if (
-        report.when == "call"
-        and report.failed
-        and report.session.config.option.fold
-    ):
+    if report.when == "call" and report.failed and report.session.config.option.fold:
         report.longrepr.chain[0][0].reprentries[0].lines.insert(0, "===MARKER1===")
         report.longrepr.chain[0][0].extraline = "===MARKER2==="
--- pytest_fold/tui.py
+++ pytest_fold/tui.py
@@ -1,7 +1,6 @@
 import itertools
 from asciimatics.screen import Screen, Canvas
 
-
 class Point:
     def __init__(self, x: int = 0, y: int = 0):
         self.x = x
@@ -10,43 +9,50 @@ class Point:
     def is_same_as(self, other):
         return self.x == other.x and self.y == other.y
 
+def main(inp_file: str="/Users/jwr003/coding/pytest-fold/pytest_fold/output.ansi"):
+    with open(inp_file, "r") as results_file:
+        results_lines = results_file.readlines()
+        fold_symbols = itertools.cycle(["▼", "▶"])
 
-with open("/Users/jwr003/coding/pytest-fold/output.ansi", "r") as results_file:
-    results_lines = results_file.readlines()
-    results = "".join(results_lines)
-    num_results_lines = len(results_lines)
-    print(num_results_lines)
-    fold_symbols = itertools.cycle(["▼", "▶"])
+        def folder(screen):
+            # Set up points denoting fold symbol, and first line of results text
+            P1 = Point(0, 0)
 
-    def folder(screen):
-        # Set up points denoting fold symbol, and first line of results text
-        P1 = Point(0, 0)
+            # Draw initially-unfolded results text ("▶")
+            screen.clear()
+            screen.print_at("▶", P1.x, P1.y)
+            screen.refresh()
 
-        # Draw initially-unfolded results text ("▶")
-        screen.clear()
-        screen.print_at("▶", P1.x, P1.y)
-        screen.refresh()
+            # main loop; looks for mouse click on folding-arrow to fold/collapse
+            # the Pytest output text; or a "q" to quit the program
+            while True:
+                event = screen.get_event()
 
-        # main loop
-        while True:
-            event = screen.get_event()
-            if event:
-                if "MouseEvent" in repr(type(event)):
-                    ClickPoint = Point(event.x, event.y)
-                    if ClickPoint.is_same_as(P1):
-                        if screen.get_from(P1.x, P1.y)[0] == ord("▶"):
-                            screen.print_at(next(fold_symbols), P1.x, P1.y)
-                            for _, result_line in enumerate(results_lines, start=1):
-                                screen.print_at(result_line, P1.x, P1.y + _)
+                if event:
+                    if "MouseEvent" in repr(type(event)):
+                        ClickPoint = Point(event.x, event.y)
+                        if ClickPoint.is_same_as(P1):
+                            if screen.get_from(P1.x, P1.y)[0] == ord("▶"):
+                                screen.print_at(next(fold_symbols), P1.x, P1.y)
+                                for _, result_line in enumerate(results_lines, start=1):
+                                    screen.print_at(result_line, P1.x, P1.y + _)
+                                    screen.refresh()
+                            else:
+                                screen.clear()
+                                screen.print_at(next(fold_symbols), P1.x, P1.y)
                                 screen.refresh()
-                        else:
-                            screen.clear()
-                            screen.print_at(next(fold_symbols), P1.x, P1.y)
-                            screen.refresh()
-                elif "KeyboardEvent" in repr(type(event)):
-                    if event.key_code in (ord("Q"), ord("q")):
-                        return
-                else:
-                    continue
 
-    Screen.wrapper(folder)
+                    elif "KeyboardEvent" in repr(type(event)):
+                        if event.key_code in (ord("Q"), ord("q")):
+                            return
+
+                    else:
+                        continue
+
+        # Kick off the asciimatics Screen instance
+        Screen.wrapper(folder)
+
+
+
+if __name__ == "__main__":
+        main("/Users/jwr003/coding/pytest-fold/pytest_fold/output2.ansi")
