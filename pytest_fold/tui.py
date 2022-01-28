821b2f171fbe81afc5a85ec3316207e6abd07a59
Jeff Wright <jeffrey.wright@wartsila.com>
2022-01-28 00:11:52 -0700

    Trying to get folded lines to show topline of section (not working yet).

--- pytest_fold/tui.py
+++ pytest_fold/tui.py
@@ -1,6 +1,7 @@
-import utils
-from math import ceil
 from pathlib import Path
+from math import ceil
+# from utils import tokenize
+from plugin import MARKER1, MARKER2
 from asciimatics.exceptions import ResizeScreenError, StopApplication
 from asciimatics.event import KeyboardEvent
 from asciimatics.parsers import AnsiTerminalParser
@@ -8,10 +9,35 @@ from asciimatics.screen import Screen
 from asciimatics.scene import Scene
 from asciimatics.widgets import Frame, TextBox, Layout, CheckBox, Button
 
+
 RESULTS_FILE = (
     "/Users/jwr003/coding/pytest-fold/output_files_to_analyze/outputvvfold.ansi"
 )
 DEBUG = True
+
+
+def tokenize(lines: str, marker1: str=MARKER1, marker2: str=MARKER2) -> list[str]:
+    sections = []
+    section_content = ""
+    marker1_seen = False
+    for line in lines:
+        if not marker1_seen:
+            if marker1 in line:
+                marker1_seen = True
+                sections.append(section_content)
+                section_content = ""
+                continue
+            section_content += line
+            continue
+        if marker1 in line:
+            continue
+        if marker2 in line:
+            sections.append(section_content)
+            section_content = ""
+            continue
+        section_content += line
+    sections.append(section_content)
+    return sections
 
 
 class ResultsData:
@@ -29,21 +55,11 @@ class ResultsData:
     def _tokenize_results(self) -> None:
         with open(self.results_file, "r") as results_file:
             results_lines = results_file.readlines()
-        self.sections = utils.tokenize(results_lines)
+        self.sections = tokenize(results_lines)
 
     def get_results(self) -> list:
         self._tokenize_results()
         return self.sections
-
-    def parse_results_ansi(self) -> list:
-        """
-        Run asciimatics ANSI parser on data to be displayed in TextBox widgets
-        """
-        parser = self.parser
-        for section in self.sections:
-            parser.reset(str(section), None)
-            self.parsed_sections.append(parser.parse())
-        return self.parsed_sections
 
 
 class ResultsLayout(Layout):
@@ -70,16 +86,17 @@ class ResultsLayout(Layout):
     def add_widgets(self) -> None:
         cb = CheckBox(text="", on_change=self._toggle_checkbox)
         self.add_widget(cb, column=0)
-        if not self.folded:
-            tb = TextBox(
-                height=self.textboxheight,
-                line_wrap=True,
-                readonly=True,
-                as_string=True,
-                parser=self.parser,
-            )
-            tb.value = self.value
-            self.add_widget(tb, column=1)
+        ht = 1 if self.folded else self.textboxheight
+        wrap = not self.folded
+        tb = TextBox(
+            height=ht,
+            line_wrap=wrap,
+            readonly=True,
+            as_string=True,
+            parser=self.parser,
+        )
+        tb.value = self.value
+        self.add_widget(tb, column=1)
 
     def _toggle_checkbox(self) -> None:
         self.folded = not self.folded
@@ -94,6 +111,7 @@ class QuitterLayout(Layout):
     """
 
     def __init__(self, screen: Screen) -> None:
+        # 4 to make room for '[ ]' plus a space; -6 to offset from end of line in frame
         super(QuitterLayout, self).__init__(columns=[4, screen.width - 6])
 
     def add_widgets(self) -> None:
@@ -113,16 +131,13 @@ class ResultsFrame(Frame):
         # sections to the ResultsFrame
         results_data = ResultsData()
         sections = results_data.get_results()
-        # parsed_generators = results_data.parse_results_ansi()
-        # for gen in parsed_generators:
-        #     parsed_section = "".join([])
 
         # First layout section: "header" info from Pytest
         self.add_layout(
             ResultsLayout(
                 screen=screen,
                 folded=False,
-                textboxheight=ceil(len(sections[0]) / screen.width) + 10,
+                textboxheight=ceil(len(sections[0]) / screen.width),
                 value=sections[0],
             )
         )
@@ -133,7 +148,7 @@ class ResultsFrame(Frame):
                 ResultsLayout(
                     screen=screen,
                     folded=True,
-                    textboxheight=ceil(len(sections[_]) / screen.width) + 10,
+                    textboxheight=ceil(len(sections[_]) / screen.width),
                     value=sections[_],
                 )
             )
@@ -143,7 +158,7 @@ class ResultsFrame(Frame):
             ResultsLayout(
                 screen=screen,
                 folded=False,
-                textboxheight=ceil(len(sections[-1]) / screen.width) + 10,
+                textboxheight=ceil(len(sections[-1]) / screen.width),
                 value=sections[-1],
             )
         )
