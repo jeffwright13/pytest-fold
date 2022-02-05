d08b6d4ea4b369fe2700ae7888adf30ccdb7b3d7
Jeff Wright <jeffrey.wright@wartsila.com>
2022-02-05 10:37:16 -0700

    Bump version: 0.5.0 => 0.6.0.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-fold",
-    version="0.5.0",
+    version="0.6.0",
     author="Jeff Wright",
     author_email="jeff.washcloth@gmail.com",
     license="MIT",
