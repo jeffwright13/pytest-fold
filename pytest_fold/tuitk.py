0701c73593dfa3c83cb3111989fcfd31aeae533e
Jeff Wright <jeffrey.wright@wartsila.com>
2022-03-20 19:00:08 -0600

    Slight progress w PyTermGui after developer fixed ANSI handling.

--- pytest_fold/tuitk.py
+++ pytest_fold/tuitk.py
@@ -1,12 +1,17 @@
+# import os, sys
+
+# file_path = "/Users/jwr003/coding/pyTermTk/TermTk"
+# sys.path.append(os.path.dirname(file_path))
+
 import faker
 import TermTk as ttk
-import sys
 
 from os import get_terminal_size
 from rich import print
 from rich.text import Text
 from pytest_fold.utils import Results, MarkedSections
 
+TERMINAL_SIZE = get_terminal_size()
 OUTPUT_SECTIONS = {
     "FIRSTLINE": "Session Start",
     "ERRORS": "Errors",
@@ -18,68 +23,51 @@ OUTPUT_SECTIONS = {
     "RAW": "Raw Output",
 }
 
-TEXT = faker.Faker().text(5000)
-TERMINAL_SIZE = get_terminal_size()
-
 
 def main():
+    test_results = Results()
+
     root = ttk.TTk()
 
-    test_results = Results()
-    unmarked_output = test_results._unmarked_output
-    marked_sections = test_results._marked_output
-    summary_text = (
-        Text.from_ansi(marked_sections.get_section("LASTLINE")["content"])
-        .markup.replace("=", "")
-        .strip()
+    top_window = ttk.TTkWindow(
+        parent=root,
+        pos=(0, 0),
+        size=(TERMINAL_SIZE.columns - 10, 3),
+        title=test_results._marked_output.get_section("LASTLINE")["content"].replace(
+            "=", ""
+        ),
+        border=False,
+    )
+    top_window.setLayout(ttk.TTkHBoxLayout())
+
+    main_frame = ttk.TTkFrame(
+        parent=root, pos=(0, 3), height=(TERMINAL_SIZE.columns - 10), border=True
     )
 
-    # top_window = ttk.TTkWindow(
-    #     parent=root,
-    #     pos=(0, 0),
-    #     size=(TERMINAL_SIZE.columns - 10, 3),
-    #     title = "Hi",,
-    #     # title = Text.from_ansi(marked_sections.get_section("LASTLINE")["content"])
-    #     # title = summary_text.print(),
-    #     # title=marked_sections.get_section("LASTLINE")["content"].replace("=", ""),
-    #     border=False,
-    # )
-    # # main_layout = ttk.TTkHBoxLayout()
-    # top_window.setLayout(ttk.TTkHBoxLayout())
-
-    results_summary_frame = ttk.TTkFrame(parent=root, pos=(0, 0), size=(TERMINAL_SIZE.columns - 10, 3), border=True)
-    results_summary_label = ttk.TTkLabel(parent=results_summary_frame)
-    results_summary_label.setText(r"""[31m==================== [31m[1m8 failed[0m, [32m4 passed[0m, [33m2 skipped[0m, [33m2 xfailed[0m, [33m2 xpassed[0m, [31m[1m3 errors[0m[31m in 2.02s[0m[31m ====================[0m""")
-
-    quit_frame = ttk.TTkFrame(parent=root, pos=(TERMINAL_SIZE.columns - 10, 0), size=(10, 3), border=True)
-    quit_button = ttk.TTkButton(parent=quit_frame, border=True, height=3)
-    quit_button.text = "Hi"
-
-    # main_frame = ttk.TTkFrame(parent=root, pos=(0, 3), height=(TERMINAL_SIZE.columns - 10), border=True)
-
-    # tab_widget = ttk.TTkTabWidget(parent=main_frame, border = False, height=4)
-    # for key, value in OUTPUT_SECTIONS.items():
-    #     if key in ["FIRSTLINE", "ERRORS", "WARNINGS_SUMMARY"]:
-    #         text = marked_sections.get_section(key)["content"]
-    #         text_edit = ttk.TTkTextEdit(parent=tab_widget)
-    #         text_edit.setText(text)
-    #         tab_widget.addTab(text_edit, f"  {value}  ")
-    #     elif key == "TERMINAL_SUMMARY":
-    #         text = marked_sections.get_section(key)["content"]
-    #         text += marked_sections.get_section("LASTLINE")["content"]
-    #         text_edit = ttk.TTkTextEdit(parent=tab_widget)
-    #         text_edit.setText(text)
-    #         tab_widget.addTab(text_edit, f"  {value}  ")
-    #     elif key == "RAW":
-    #         text = unmarked_output
-    #         text_edit = ttk.TTkTextEdit(parent=tab_widget)
-    #         text_edit.setText(text)
-    #         tab_widget.addTab(text_edit, f"  {value}  ")
-
-    #     else:
-    #         tab_widget.addTab(ttk.TTkTree(border=False, title=value), f"  {value}  ")
-
-
+    tab_widget = ttk.TTkTabWidget(parent=main_frame, border=False, height=4)
+    for key, value in OUTPUT_SECTIONS.items():
+        if key in ["FIRSTLINE", "ERRORS", "WARNINGS_SUMMARY"]:
+            text = test_results._marked_output.get_section(key)["content"]
+            text_edit = ttk.TTkTextEdit(parent=tab_widget)
+            text_edit.setText(text)
+            tab_widget.addTab(text_edit, f"  {value}  ")
+        elif key == "TERMINAL_SUMMARY":
+            text = test_results._marked_output.get_section(key)["content"]
+            text += test_results._marked_output.get_section("LASTLINE")["content"]
+            text_edit = ttk.TTkTextEdit(parent=tab_widget)
+            text_edit.setText(text)
+            tab_widget.addTab(text_edit, f"  {value}  ")
+        elif key == "RAW":
+            text = test_results._unmarked_output
+            text_edit = ttk.TTkTextEdit(parent=tab_widget)
+            text_edit.setText(text)
+            tab_widget.addTab(text_edit, f"  {value}  ")
+        elif key == "FAIL":
+            text = ""
+            tests = test_results.failures
+            for test in tests:
+                text += test
+            tab_widget.addTab(text_edit, f"  {value}  ")
 
     root.mainloop()
 
---
+++ tests/test_show_warnings.py
@@ -0,0 +1,10 @@
+import warnings
+
+
+def api_v1():
+    warnings.warn(UserWarning("api v1, should use functions from v2"))
+    return 1
+
+
+def test_one():
+    assert api_v1() == 1
